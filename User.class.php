85d697bca3f0ad246318117cac68f434bcf5fa32
Nikodem Nowitzki <nikodem.nowitzki@teb.edu.pl>
2022-11-16 11:18:39 +0100

    COnfig db in user class

--- User.class.php
+++ User.class.php
@@ -3,22 +3,30 @@ class User {
     private $db;
     private int $id;
     private string $login;
-    private string $passwordHash;
+    private string $password;
     private string $firstName;
     private string $lastName;
 
     public function __construct(string $login, string $password) {
         $this->login = $login;
-        $this->password_hash = password_hash($password, PASSWORD_ARGON2I);
+        $this->password = $password;
         global $db;
         $this->db = &$db;
     }
 
     public function isAUth() : bool {
-
+        if((isset($this->id)) && $this->id != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public function login() {
-
+        $q = "SELECT * FROM user WHERE login = ?";
+        $preparedQuery = $this->db->prepare($q);
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
     }
     public function logout() {
 
