fb5754bb503031ba444b64cd698ad5b807375f50
Nikodem Nowitzki <nikodem.nowitzki@teb.edu.pl>
2022-11-16 11:05:56 +0100

    add db connection for features uses

--- User.class.php
+++ User.class.php
@@ -1,5 +1,6 @@
 <?php
 class User {
+    private $db;
     private int $id;
     private string $login;
     private string $passwordHash;
@@ -8,7 +9,9 @@ class User {
 
     public function __construct(string $login, string $password) {
         $this->login = $login;
-        $this->password_hash = password_hash($password, PASSWORD_ARGON2I)
+        $this->password_hash = password_hash($password, PASSWORD_ARGON2I);
+        global $db;
+        $this->db = &$db;
     }
 
     public function isAUth() : bool {
