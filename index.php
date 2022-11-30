7428df191160906fd2139f54aca90cb2561d7f7e
Nikodem Nowitzki <nikodem.nowitzki@teb.edu.pl>
2022-11-30 11:19:06 +0100

    logowanie z routingiem

--- index.php
+++ index.php
@@ -11,8 +11,31 @@ Route::add('/',function () {
 });
 
 Route::add('/login', function() {
-    echo "Strona logowania";
+    global $twig;
+    $twig->display('login.html.twig');
 });
+
+Route::add('/login', function() {
+
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+
+    
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            //echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            //echo "Błędny login lub hasło";
+            $twig->display('message.html.twig', ['message'=> "Błędny login lub hasło"]);
+        }
+    } else {
+        die("Uzupełnij dane");
+    }
+}, 'post');
 
 Route::run('/loginForm');
 ?>
