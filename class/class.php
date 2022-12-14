b4c078cb47500919358227348e5a7f74afc4e413
Nikodem Nowitzki <nikodem.nowitzki@teb.edu.pl>
2022-12-14 11:15:40 +0100

    everything works

--- class/class.php
+++ class/class.php
@@ -7,34 +7,52 @@ class User {
     private string $firstName;
     private string $lastName;
 
-        public function __construct(string $login, string $password) {
-            $this->login = $login;
-            $this->password = $password;
-            $this->firstName = "";
-            $this->lastName = "";
-            global $db;
-            $this->db = &$db;
-        }
-        
-        public function register() : bool {
-            $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
-            $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-            $preparedQuery = $this->db->prepare($query);
-            $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstName, $this->lastName);
-            $result = $preparedQuery->execute();
-            return $result;
-        }
+    public function __construct(string $login, string $password) {
+        $this->login = $login;
+        $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
+        global $db;
+        $this->db = &$db;
+    }
+    public function __serialize() : array {
+        return array(   
+                        'id' => $this->id,
+                        'login' => $this->login,
+                        'password' => $this->password,
+                        'firstName' => $this->firstName,
+                        'lastName' => $this->lastName,
+                    );
+    }
+    public function __unserialize(array $data) {
+        $this->id = $data['id'];
+        $this->login = $data['login'];
+        $this->password = $data['password'];
+        $this->firstName = $data['firstName'];
+        $this->lastName = $data['lastName'];
+        global $db;
+        $this->db = &$db;
+    } 
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+    }
 
-        public function login() : bool {
-            $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
-            $preparedQuery = $this->db->prepare($query);
-            $preparedQuery->bind_param('s', $this->login); 
-            $preparedQuery->execute();
-            $result = $preparedQuery->get_result();
-            if($result->num_rows == 1) {
-                $row = $result->fetch_assoc();
-                 $passwordHash = $row['password'];
-                if(password_verify($this->password, $passwordHash)) {
+    public function login() : bool {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
                 $this->id = $row['id'];
                 $this->firstName = $row['firstName'];
                 $this->lastName = $row['lastName'];
@@ -55,7 +73,15 @@ class User {
     public function getName() : string {
         return $this->firstName . " " . $this->lastName;
     }
-
+    public function save() : bool {
+        $q = "UPDATE user SET
+                firstName = ?,
+                lastName = ?
+                WHERE id = ?";
+        $preparedQuery = $this->db->prepare($q);
+        $preparedQuery->bind_param("ssi", $this->firstName, $this->lastName, $this->id);
+        return $preparedQuery->execute();
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -5,37 +5,111 @@ use Steampixel\Route;
 require_once('config.php');
 require_once('class/class.php');
 
-Route::add('/',function () {
-        //ten kod wywoła się na głównej stronie
-        echo "Strona główna";
+session_start();
+
+Route::add('/', function() {
+    global $twig;
+    $v = array();
+    if(isset($_SESSION['auth']))
+        if($_SESSION['auth']) {
+            //jesteśmy zalogowani
+            $user = $_SESSION['user'];
+            $v['user'] = $user;
+            
+        }
+    $twig->display('home.html.twig', $v);
+    //echo "<pre>";
+    //var_dump($_SESSION);
 });
 
-Route::add('/login', function() {
+Route::add('/login', function() { 
     global $twig;
     $twig->display('login.html.twig');
 });
 
 Route::add('/login', function() {
-
     global $twig;
     if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
-
-    
         $user = new User($_REQUEST['login'], $_REQUEST['password']);
         if($user->login()) {
-            //echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+            // tu jest już poprawnie zalogowany użytkownik
+            $_SESSION['auth'] = true;
+            $_SESSION['user'] = $user;
             $v = array(
                 'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
             );
             $twig->display('message.html.twig', $v);
         } else {
-            //echo "Błędny login lub hasło";
-            $twig->display('message.html.twig', ['message'=> "Błędny login lub hasło"]);
+            $twig->display('login.html.twig', 
+                                ['message' => "Błędny login lub hasło"]);
         }
     } else {
-        die("Uzupełnij dane");
+        die("Nie otrzymano danych");
     }
 }, 'post');
+
+Route::add('/register', function() {
+    global $twig;
+    $twig->display('register.html.twig');
+});
+Route::add('/register', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
+            || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+                //podano pusty string jako jedną z wymaganych wartości
+                $twig->display('register.html.twig', 
+                                ['message' => "Nie podano wymaganej wartości"]);
+                exit();
+            }
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        $user->setFirstName($_REQUEST['firstName']);
+        $user->setLastName($_REQUEST['lastName']);
+        if($user->register()) {
+            //echo "Zarejestrowano poprawnie";
+            $twig->display('message.html.twig', 
+                                ['message' => "Zarejestrowano poprawnie"]);
+        } else {
+            //echo "Błąd rejestracji użytkownika";
+            $twig->display('register.html.twig', 
+                                ['message' => "Błąd rejestracji użytkownika"]);
+        }
+    } else {
+        die("Nie otrzymano danych");
+    }
+}, 'post');
+
+Route::add('/logout', function() {
+    global $twig;
+    session_destroy();
+    $twig->display('message.html.twig', 
+                                ['message' => "Wylogowano poprawnie"]);
+});
+
+Route::add('/profile', function() {
+    global $twig;
+    $user = $_SESSION['user'];
+    //pobieramy imię i nazwisko rozdzielone spacją
+    $fullName = $user->getName();
+    $fullName = explode(" ", $fullName); // "Imię nazwisko" => array ("Imię", "Nazwisko");
+    $v = array( 'user'      => $user,
+                'firstName' => $fullName[0],
+                'lastName'  => $fullName[1],
+            );
+    $twig->display('profile.html.twig', $v);
+});
+Route::add('/profile', function() {
+    global $twig;
+    if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
+        $user = $_SESSION['user'];
+        $user->setFirstName($_REQUEST['firstName']);
+        $user->setLastName($_REQUEST['lastName']);
+        $user->save();
+        $twig->display('message.html.twig', 
+                                ['message' => "Zapisano zmiany w profilu"]);
+    }
+}, "post");
+
 
 Route::run('/loginForm');
 ?>
